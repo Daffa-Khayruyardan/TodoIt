7e8ddf86e6e5fe341919a75d0ece4c3126c9b4a6
daffa-khayru <daffakhayru@student.telkomuniversity.ac.id>
2024-05-13 16:09:17 +0700

    update client side

--- client/pages/AddTodo.jsx
+++ client/pages/AddTodo.jsx
@@ -1,16 +1,24 @@
+// import some packages
+import { Link } from "react-router-dom";
+
 const AddTodo = () => {
     return(
         <div className="xl:flex xl:justify-center xl:items-center xl:bg-silverLight xl:flex-1">
             {/* form container */}
             <form action="" className="xl:shadow-md xl:bg-white xl:w-96 xl:h-96">
                 {/* form title */}
-                <h1 className="xl:pt-[1em] xl:pl-[1em] xl:text-2xl">Add New</h1>
+                <h1 className="xl:pt-[1em] xl:pl-[1.5em] xl:text-2xl">Add New</h1>
 
                 {/* form description */}
-                <p className="xl:pt-[1em] xl:pl-[1.4em]">Add new activities enter here</p>
+                <p className="xl:pt-[1em] xl:pl-[2.1em]">Add new activities enter here</p>
 
                 {/* input field */}
-                <input type="text" className="xl:bg-greenLightDying xl:focus:outline-none" />
+                <input type="text" className="xl:ml-[1.9em] xl:mt-[1em] xl:w-[82%] xl:bg-greenLightDying xl:focus:outline-none" />
+
+                {/* button container */}
+                <div>
+                    <Link></Link>
+                </div>
             </form>
         </div>
     )
